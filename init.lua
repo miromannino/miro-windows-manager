9152e572016742ef490076d0b8f5852c1c4cbc62
Miro Mannino <miro.mannino@gmail.com>
2016-11-06 00:03:47 +0400

    example.gif

--- init.lua
+++
@@ -1,5 +0,0 @@
-hyper = {"ctrl", "alt", "cmd"}
-hypershift = {"ctrl", "alt", "cmd", "shift"}
-
-require('watcher')
-require('position')
--- watcher.lua
+++
@@ -1,8 +0,0 @@
--- --------------------------------------
--- Watcher to load the configuration in case of changes
--- --------------------------------------
-function reload_config(files)
-    hs.reload()
-end
-hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reload_config):start()
-hs.alert.show("Config loaded")
