7a6c82f066f72b9d8c89d57016f59425a039ef77
Matthew Fallshaw <m@fallshaw.me>
2018-05-30 21:39:25 +1000

    bugfix: hs.grid rounding issues were preventing fullscreen sizing, fixed

--- MiroWindowsManager.spoon/init.lua
+++ MiroWindowsManager.spoon/init.lua
@@ -39,38 +39,48 @@ obj.__index = obj
 
 -- Metadata
 obj.name = "MiroWindowsManager"
-obj.version = "1.1"
+obj.version = "1.2"
 obj.author = "Miro Mannino <miro.mannino@gmail.com>"
 obj.homepage = "https://github.com/miromannino/miro-windows-management"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
-obj._logger = hs.logger.new(obj.name)
-local logger = obj._logger
+local logger = hs.logger.new(obj.name)
+obj._logger = logger  -- make logger available so users can turn up the volume!
 logger.i("Loading ".. obj.name)
 
 
 -- ### Utilities
 function string.titleCase(str)
+  -- ## WARNING: Global change ##
   return (str:gsub('^%l', string.upper))
 end
-local function expect_argument_to_be_in_table(argument, to_be_in)
+local expect = {}
+function expect.argument_to_be_in_table(argument, to_be_in)
   assert(
     hs.fnutils.contains(to_be_in, argument),
     'Expected "'.. hs.inspect(argument) ..'" to be one of '.. hs.inspect(to_be_in)
   )
 end
-local function expect_argument_to_be_in_table_or_nil(argument, to_be_in)
+function expect.argument_to_be_in_table_or_nil(argument, to_be_in)
   assert(
     hs.fnutils.contains(to_be_in, argument) or argument == nil,
     'Expected "'.. hs.inspect(argument) ..'" to be one of '.. hs.inspect(to_be_in)
   )
 end
-local function expect_truthy(argument, expression)
+function expect.truthy(argument, expression)
   assert(
     argument,
     'Expected truthyness from '.. hs.inspect(expression)
   )
 end
+local function round(num)
+  if num >= 0 then
+    return math.floor(num+.499999999)
+  else
+    return math.ceil(num-.499999999)
+  end
+end
+obj._round = round
 
 
 -- ## Public variables
@@ -80,7 +90,9 @@ end
 --- The sizes that the window can have.  
 --- The sizes are expressed as dividend of the entire screen's size.  
 --- For example `{2, 3, 3/2}` means that it can be 1/2, 1/3 and 2/3 of the total
---- screen's size.
+--- screen's size.  
+--- Ensuring that these numbers all divide both dimensions of
+--- MiroWindowsManager.GRID to give integers makes everything work better.
 obj.sizes = {2, 3, 3/2}
 
 --- MiroWindowsManager.fullScreenSizes
@@ -88,18 +100,31 @@ obj.sizes = {2, 3, 3/2}
 --- The sizes that the window can have in full-screen.  
 --- The sizes are expressed as dividend of the entire screen's size.  
 --- For example `{1, 4/3, 2}` means that it can be 1/1 (hence full screen), 3/4
---- and 1/2 of the total screen's size
+--- and 1/2 of the total screen's size.  
+--- Ensuring that these numbers all divide both dimensions of
+--- MiroWindowsManager.GRID to give integers makes everything work better.  
+--- Special: Use 'c' for the original size and shape of the window before
+--- starting to move it, but centered.
 obj.fullScreenSizes = {1, 4/3, 2}
 
+-- Comment: Lots of work here to save users a little work. Previous versions
+-- required users to call MiroWindowsManager:start() every time they changed
+-- GRID. The metatable work here watches for those changes and does the work
+-- :start() would have done.
+--
 -- Ensure changes to GRID update hs.grid
 --   Prevent obj.GRID from being replaced
-local _grid_store_yeeV5hiG = {}  -- we store the real GRID here
+local _grid_store_yeeV5hiG = {}  -- we'll store the real GRID here
 setmetatable(obj, {
-  __index = function(t,k)
+  __index = function(t,k)  -- if code reads obj, use this function (eg. `obj.sizes`)
+    -- if code accesses obj.GRID, return the real GRID we created above,
+    -- otherwise access obj as normal
     if k == 'GRID' then return _grid_store_yeeV5hiG else return rawget(t,k) end end,
   __newindex =
-    function(t,k,v)
+    function(t,k,v)  -- if code writes to obj, use this function (eg. `obj.sizes = {2, 3}`)
       if k == 'GRID' then
+      -- if code assigns to obj.GRID (`obj.GRID = ...`), don't overwrite our
+      -- real GRID, otherwise access obj as normal
         assert(type(v)=='table',rawget(obj,'name')..".GRID must be a table.")
         -- assign the assigned table's content to our real GRID table
         for kk,vv in pairs(v) do obj.GRID[kk] = vv end
@@ -109,16 +134,20 @@ setmetatable(obj, {
     end,
 })
 --   Update hs.grid after relevant changes to obj.GRID
-local _grid_value_store_Oocaeyim = {}  -- we store the real GRID values here
+local _grid_value_store_Oocaeyim = {}  -- we store the real GRID *values* here
 local m = {
   __newindex =
-    function(t,k,v)
-      rawset(_grid_value_store_Oocaeyim,k,v)
+    function(t,k,v)  -- if code assigns to our real GRID…
+      rawset(_grid_value_store_Oocaeyim,k,v) -- do the assignment, then…
       if hs.fnutils.contains({'w','h'}, k) then
+        -- update hs.grid with hs.grid.setGrid, so the user doesn't have to
         hs.grid.setGrid(tostring(_grid_value_store_Oocaeyim.w or 0) ..  'x' ..
           tostring(_grid_value_store_Oocaeyim.h or 0))
+        logger.i("Updated hs.grid to ".. hs.inspect(_grid_value_store_Oocaeyim))
       elseif k == 'margins' then
+        -- update hs.grid with hs.grid.setMargins, so the user doesn't have to
         hs.grid.setMargins(v)
+        logger.i("Updated hs.grid to ".. hs.inspect(_grid_value_store_Oocaeyim))
       elseif hs.fnutils.contains({'MARGINX','MARGINY'}, k) then
         -- LEGACY
         if k == 'MARGINX' then
@@ -129,8 +158,8 @@ local m = {
             hs.geometry(tostring(rawget(_grid_value_store_Oocaeyim, 'margins').w or 0) ..'x'..
             tostring(v))
         end
+        logger.i("Updated hs.grid to ".. hs.inspect(_grid_value_store_Oocaeyim))
       end
-      logger.i("Updated hs.grid to ".. hs.inspect(_grid_value_store_Oocaeyim))
     end,
   __index =
     function(t,k) return rawget(_grid_value_store_Oocaeyim, k) end,
@@ -139,13 +168,19 @@ setmetatable(_grid_store_yeeV5hiG, m)
 
 --- MiroWindowsManager.GRID
 --- Variable
---- The screen's grid size.
-obj.GRID = { w = 24, h = 24, margins = hs.geometry("0x0") }
+--- The screen's grid size.  
+--- Ensuring that the numbers in MiroWindowsManager.sizes and
+--- MiroWindowsManager.fullScreenSizes divide these numbers to give integers
+--- makes everything work better.  
+obj.GRID = { w = 24, h = 24, margins = hs.geometry.point(0,0) }
+function obj.GRID.cell()
+  return hs.geometry(obj.GRID.margins, hs.geometry.size(obj.GRID.w, obj.GRID.h))
+end
 
 
 --- MiroWindowsManager.pushToNextScreen
 --- Variable
---- If `move`d past the screen edge, jump to next screen?
+--- If `move`d past the screen edge, jump to next screen? (true|false)
 obj.pushToNextScreen = false
 
 
@@ -156,11 +191,12 @@ obj.pushToNextScreen = false
 -- Window moves and their relationships
 local directions = { 'up', 'down', 'left', 'right' }
 local directions_rel = {
-  up =    { opp = 'down',  grow = 'taller', dim = 'h', pos = 'y', home = 0 },
-  down =  { opp = 'up',    grow = 'taller', dim = 'h', pos = 'y', home = obj.GRID.h },
-  left =  { opp = 'right', grow = 'wider',  dim = 'w', pos = 'x', home = 0 },
-  right = { opp = 'left',  grow = 'wider',  dim = 'w', pos = 'x', home = obj.GRID.w },
+  up =    { opp = 'down',  grow = 'taller', dim = 'h', pos = 'y', home = function() return 0 end },
+  down =  { opp = 'up',    grow = 'taller', dim = 'h', pos = 'y', home = function() return obj.GRID.h end },
+  left =  { opp = 'right', grow = 'wider',  dim = 'w', pos = 'x', home = function() return 0 end },
+  right = { opp = 'left',  grow = 'wider',  dim = 'w', pos = 'x', home = function() return obj.GRID.w end },
 }
+obj._directions_rel = directions_rel
 -- Window growths and their relationships
 local growths = { 'taller', 'shorter', 'wider', 'thinner' }
 local growths_rel = {
@@ -169,6 +205,7 @@ local growths_rel = {
   wider   = { opp = 'thinner', dim = 'w', pos = 'x', side = 'left',  sticky_bound_fix = false },
   thinner = { opp = 'wider',   dim = 'w', pos = 'x', side = 'right', sticky_bound_fix = true  },
 }
+obj._growths_rel = growths_rel
 
 -- ### Internal state
 
@@ -178,6 +215,7 @@ hs.fnutils.each(hs.fnutils.concat(directions, growths), function(move)
   obj._pressed[move] = false
   obj._press_timers[move] = hs.timer.doAfter(1, function() obj._pressed[move] = false end)
 end)
+obj._originalPositionStore = {}
 
 local function register_press(direction)
   obj._pressed[direction] = true
@@ -194,8 +232,8 @@ end
 -- ### Internal convenience functions
 
 -- Accessor for functions on the frontmost window
-obj._frontmost = {}
-local frontmost = obj._frontmost
+local frontmost = {}
+obj._frontmost = frontmost
 
 -- An hs.window for the frontmost window
 function frontmost.window()
@@ -224,7 +262,7 @@ end
 --- Returns:
 ---  * The MiroWindowsManager object
 function obj:move(side)
-  expect_argument_to_be_in_table(side, directions)
+  expect.argument_to_be_in_table(side, directions)
 
   if self:boundQ(side) and not self.pushToNextScreen then
     logger.i("`self.pushToNextScreen` == false so not moving to ".. side .." screen.")
@@ -254,7 +292,7 @@ hs.fnutils.each(directions,  -- up(), down, left, right
 --- Returns:
 ---  * The MiroWindowsManager object
 function obj:grow(growth)
-  expect_argument_to_be_in_table(growth, growths)
+  expect.argument_to_be_in_table(growth, growths)
 
   register_press(growth)
   if pressedQ(growths_rel[growth].opp) then 
@@ -301,7 +339,7 @@ hs.fnutils.each(growths,  -- taller(), shorter, wider, thinner
 --- Returns:
 ---  * The MiroWindowsManager object
 function obj:growFully(growth)
-  expect_argument_to_be_in_table(growth, growths)
+  expect.argument_to_be_in_table(growth, growths)
 
   local cell = frontmost.cell()
   cell[growths_rel[growth].pos] = 0
@@ -331,7 +369,7 @@ function obj:growWidest() return self:growFully('wider') end
 function obj:go(move)
   if move == 'fullscreen' then return self:goFullscreen() end
 
-  expect_argument_to_be_in_table(move, directions)
+  expect.argument_to_be_in_table(move, directions)
 
   register_press(move)
   if pressedQ(directions_rel[move].opp) then 
@@ -341,18 +379,13 @@ function obj:go(move)
     self:growFully(directions_rel[move].grow) -- full width/height
   else
     local cell = frontmost.cell()
-    local seq = self:seqQ(move)
-    local log_info = "We're at ".. move .." sequence ".. tostring(seq) .."(".. hs.inspect(cell) ..")"
+    local seq = self:seqQ(move)  -- current sequence index or 0 if out of sequence
+    local log_info = "We're at ".. move .." sequence ".. tostring(seq) .." (".. cell.string ..")"
     seq = seq % #self.sizes  -- if #self.sizes then 0
     log_info = log_info .. ", so moving to sequence " .. tostring(seq + 1)
     logger.i(log_info)
-    cell[directions_rel[move].dim] = self.GRID[directions_rel[move].dim] / self.sizes[seq + 1]
-    if hs.fnutils.contains({'left', 'up'}, move) then
-      cell[directions_rel[move].pos] = directions_rel[move].home
-    else
-      cell[directions_rel[move].pos] = directions_rel[move].home - cell[directions_rel[move].dim]
-    end
-    self._setPosition(cell)
+
+    cell = self:setToSeq(move, seq + 1)
   end
   return self
 end
@@ -372,74 +405,43 @@ hs.fnutils.each(directions,  -- goUp(), goDown, goLeft, goRight
 ---  * The MiroWindowsManager object
 function obj:goFullscreen()
   local cell
-  if not self:centeredQ() then
-    cell = frontmost.cell()
-    cell.x = (self.GRID.w - cell.w) / 2
-    cell.y = (self.GRID.h - cell.h) / 2
-    self._setPosition(cell)
-    if cell == frontmost.cell() then
-      -- we didn't move, assume a rounding error
-      self._setPosition(self:seqFullCell(1))
-    else
-      logger.i("Not centered, so centering (".. hs.inspect(cell) ..")")
-    end
-  else
-    local seq = self:seqFullQ()
-    local log_info = "We're at fullscreen sequence ".. tostring(seq) .."(".. hs.inspect(frontmost.cell()) ..")"
-    seq = seq % #self.fullScreenSizes  -- if #self.fullScreenSizes then 0
-    log_info = log_info .. ", so moving to sequence " .. tostring(seq + 1)
-    logger.i(log_info)
-    cell = self:seqFullCell(seq + 1)  -- next in sequence
-    self._setPosition(cell)
+  local seq = self:seqFullQ()  -- current sequence index or 0 if out of sequence
+  local log_info = "We're at fullscreen sequence ".. tostring(seq) .." (".. frontmost.cell().string .."), so"
+
+  if hs.fnutils.contains(self.fullScreenSizes, 'c') and seq == 0 then
+    -- We're out of the sequence, so store the current window position
+    obj._originalPositionStore[frontmost.window():id()] = frontmost.cell()
+    log_info = log_info .." remembering position then"
   end
+
+  seq = seq % #self.fullScreenSizes  -- if #self.fullScreenSizes then 0
+  log_info = log_info .. " moving to sequence " .. tostring(seq + 1)
+  logger.i(log_info)
+
+  cell = self:seqFullCell(seq + 1)  -- next in sequence
+  self._setPosition(cell)
+
   return self
 end
 
 
 -- ## Public undocumented
 
--- Query fullscreen sequence - 0 means out of sequence
-function obj:seqFullQ()
-  local cell = frontmost.cell()
-  for i = 1,#self.fullScreenSizes do
-    if cell == self:seqFullCell(i) then
-      return i
-    end
-  end
-  return 0
-end
-
 -- Query whether window is centered
 function obj:centeredQ()
   local cell = frontmost.cell()
-  local h_center = cell.w + 2 * cell.x == self.GRID.w
-  local v_center = cell.h + 2 * cell.y == self.GRID.h
-  return h_center and v_center
+  return cell.w + 2 * cell.x == self.GRID.w and
+         cell.h + 2 * cell.y == self.GRID.h
 end
 
--- hs.grid cell for fullscreen sequence `seq`
-function obj:seqFullCell(seq)
-  local size = {
-    x = self.GRID.w / self.fullScreenSizes[seq],
-    y = self.GRID.h / self.fullScreenSizes[seq],
-  }
-  local offset = {
-    x = (self.GRID.w - size.x) / 2,
-    y = (self.GRID.h - size.y) / 2,
-  }
-  local cell = hs.geometry({
-    x = offset.x,
-    y = offset.y,
-    w = size.x,
-    h = size.y,
-  })
+local function snap_to_grid(cell)
+  hs.fnutils.each({'w','h','x','y'}, function(d) cell[d] = round(cell[d]) end)
   return cell
 end
 
-
 -- Query sequence for `side` - 0 means out of sequence
 function obj:seqQ(side)
-  expect_argument_to_be_in_table(side, directions)
+  expect.argument_to_be_in_table(side, directions)
 
   if self:boundQ(side) then
     local dim = directions_rel[side].dim
@@ -458,13 +460,19 @@ hs.fnutils.each(directions,
 
 -- Set sequence for `side`
 function obj:setToSeq(side, seq)
-  expect_argument_to_be_in_table(side, directions)
-  expect_truthy(type(seq) == 'number', "type(seq) == 'number'")
-  expect_truthy(seq ~= 0 and seq <= #self.sizes, "seq ~= 0 and seq <= #self.sizes")
+  expect.argument_to_be_in_table(side, directions)
+  expect.truthy(type(seq) == 'number', "type(seq) == 'number'")
+  expect.truthy(seq ~= 0 and seq <= #self.sizes, "seq ~= 0 and seq <= #self.sizes")
 
   local cell = frontmost.cell()
-  cell[directions_rel[side].pos] = directions_rel[side].home
-  cell[directions_rel[side].dim] = self.sizes[seq]
+  cell[directions_rel[side].dim] = self.GRID[directions_rel[side].dim] / self.sizes[seq]
+  if hs.fnutils.contains({'left', 'up'}, side) then
+    cell[directions_rel[side].pos] = directions_rel[side].home()
+  else
+    cell[directions_rel[side].pos] = directions_rel[side].home() - cell[directions_rel[side].dim]
+  end
+
+  cell = snap_to_grid(cell)
   self._setPosition(cell)
   return self
 end
@@ -475,7 +483,7 @@ hs.fnutils.each(directions,  -- setToUpSeq(seq), setToDownSeq, setToLeftSeq, set
 
 -- Query whether window is bound to `side` (is touching that side of the screen)
 function obj:boundQ(side)
-  expect_argument_to_be_in_table(side, directions)
+  expect.argument_to_be_in_table(side, directions)
 
   local cell = frontmost.cell()
   if side == 'up' then
@@ -493,13 +501,49 @@ hs.fnutils.each(directions,  -- upBoundQ(), downBoundQ, leftBoundQ, rightBoundQ 
     obj[side ..'BoundQ'] = function(self) return  self:boundQ(side) end
   end )
 
+
+-- Query fullscreen sequence - 0 means out of sequence
+function obj:seqFullQ()
+  local cell = frontmost.cell()
+  for i = 1,#self.fullScreenSizes do
+    if cell == self:seqFullCell(i) then
+      return i
+    end
+  end
+  return 0
+end
+
+-- hs.grid cell for fullscreen sequence `seq`
+function obj:seqFullCell(seq)
+  local seq_factor = self.fullScreenSizes[seq]
+
+  local cell, pnt, size
+  if seq_factor ~= 'c' then
+    size = hs.geometry.size(
+      self.GRID.w / seq_factor,
+      self.GRID.h / seq_factor
+    )
+    pnt = hs.geometry.point(
+      (self.GRID.w - size.w) / 2,
+      (self.GRID.h - size.h) / 2
+    )
+    cell = hs.geometry(pnt, size)
+  else
+    cell =
+      obj._originalPositionStore[frontmost.window():id()] or self.GRID.cell()
+    cell.center = self.GRID.cell().center
+  end
+  return snap_to_grid(cell)
+end
+
+
 -- Set window to cell
 function obj._setPosition(cell)
-  expect_truthy(
+  expect.truthy(
     type(cell) == 'table' and type(cell.type) == 'function' and cell:type() == 'rect',
     "type(cell) == 'table' and type(cell.type) == 'function' and cell:type() == 'rect'")
 
-  local win = hs.window.frontmostWindow()
+  local win = frontmost.window()
   hs.grid.set(win, cell, win:screen())
 end
 
